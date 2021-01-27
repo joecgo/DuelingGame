6019bb2ab276206375a45ea8250359555e2fbb5e
Joe <joecgo@gmail.com>
2021-01-26 19:45:33 -0800

    Adding models for kevin shield and sword to project

---
+++ Source/DuelingGame/DuelingGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DuelingGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUELINGGAME_API ADuelingGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
